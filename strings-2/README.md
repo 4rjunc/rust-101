e292dca4e2eb7b7d36eb87af32426c6ed1b83de4
4rjunc <arjunmundamani@gmail.com>
2024-09-01 18:37:04 +0530

    Added string actions

--- strings-2/README.md
+++ strings-2/README.md
@@ -1,6 +1,18 @@
 # few more concepts
 
-int -> texts are done by UTF-8
+texts are encoded using UTF-8
 
 ASCII - can represent one character in one byte.
 UTF-8 - one character can range from one byte to 4 bytes.
+
+first byte represent number of characters
+
+0xxxxxxx
+110xxxxx 10xxxxxx
+1110xxxx 10xxxxxx 10xxxxxx
+11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
+types of strings
+
+- &str
+- String
--- strings-2/src/main.rs
+++ strings-2/src/main.rs
@@ -1,3 +1,59 @@
 fn main() {
-    println!("Hello, world!");
+    let s1: &str = "Hello World 🦀";
+    let s2: String = String::from("Hello World! 🦀");
+    let s3: String = "Hello World".to_string();
+    let s4: String = "Hello World".to_owned();
+    let s5: &str = &s4[3..=6]; // points the part of the string from 3rd character to 6th character
+
+    println!("s5: {}", s5);
+
+    let mut name: String = String::from("Arjun");
+    //pushing characters
+    name.push_str(" Mundamani");
+    println!("name: {}", name);
+    // replace characters
+    name.replace_range(5.., " C"); // replace from 5th character till end 
+    println!("name: {}", name);
+    //concat.
+    let fruit_1: String = String::from("Jack");
+    let fruit_2: String = String::from(" Fruit");
+    let fruit_name: String = fruit_1 + &fruit_2; // method 1
+    println!("fruit_name: {}", fruit_name);
+
+    let word1: String = String::from("The");
+    let word2: String = String::from("Monkey");
+    let word3: String = String::from("Is");
+    let word4: String = String::from("Crying");
+    // method 2 less efficent than + operator
+    let sentence: String = format!("{}-{}-{}-{}", word1, word2, word3, word4); 
+    println!("sentence: {}", sentence);
+
+    let word5: String = ["i", "am", "a", "good", "boy"].concat();
+    let word6: &str = concat!("Hi","How", "Are", "you?");
+
+    let word7: String = String::from("Nice");
+    let sentence2: String = word7 + "Car"; // String must be the first
+
+    // indexing into the string
+    let s1: &str = "🦀the🦀🦀🦀";
+    let s2: &str = &s1[0..4];
+    println!("s2: {}", s2);
+
+    //iterating in a string
+    //iterating bytes 
+    for b in "MUNDAMANI".bytes() {
+        println!("{}",b);
+    }
+    //iterating chars 
+    for c in "ARJUN".chars(){
+        println!("{}", c);
+    }
+    
+    let fn_word = "gm";
+    println!("my_function: {}", my_function(fn_word));
+    println!("my_function(&s2):{}", my_function(&s2));
+}
+
+fn my_function(a: &str) -> String{
+    return format!("{} !",a);
 }

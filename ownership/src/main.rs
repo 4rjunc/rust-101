6d9394dd7428738872a57b606142eb426a1cb3e9
4rjunc <arjunmundamani@gmail.com>
2024-09-17 19:35:10 +0530

    Added some example of ownership passign

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello World");
+
+    take_ownership(s);
+
+    let x = 5;
+    make_a_copy(x);
+    println!("x:{}", x);
+    // this print is invalid since the ownership of s is tranfered to the fn.
+    //println!("s: {}", s);
+}
+
+fn take_ownership(str: String){
+    println!("str: {}", str);
+}
+
+fn make_a_copy(num: i32){
+    println!("num:{}", num);
 }

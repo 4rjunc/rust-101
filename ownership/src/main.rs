bb33c75bd8335b60864991eaec27f00bfa4dddaf
4rjunc <arjunmundamani@gmail.com>
2024-09-17 19:39:40 +0530

    upload message, len

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -8,8 +8,18 @@ fn main() {
     println!("x:{}", x);
     // this print is invalid since the ownership of s is tranfered to the fn.
     //println!("s: {}", s);
+    
+    //function returning multiple values
+    let message = String::from("Im Batman");
+    let (msg, len) = calculate_msg(message);
+    println!("message:{} has length:{}", msg, len);
 }
 
+fn calculate_msg(msg: String) -> (String, usize){
+    let length = msg.len();
+
+    (msg, length)
+}
 fn take_ownership(str: String){
     println!("str: {}", str);
 }

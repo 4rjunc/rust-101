d0ac3657ccbf5952bdbd53510b75ea2f0e425a4b
4rjunc <arjunmundamani@gmail.com>
2024-09-29 10:39:25 +0530

    Completed Rust

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -1,17 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let message: String = String::from("Hello Arjun");
+    let word = first_word(&message);
+
+    println!("First Word: {}", word);
+
+
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[0..3];
+    assert_eq!(slice, &[2,3]);
 }
 
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' '{
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
-

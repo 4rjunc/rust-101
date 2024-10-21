d29f9be1c872102da8dd3e3e15909c2febaa4dc6
4rjunc <arjunmundamani@gmail.com>
2024-10-21 19:27:13 +0530

    ENUMS

--- enums_and_patternmatching/src/main.rs
+++ enums_and_patternmatching/src/main.rs
@@ -1,14 +1,11 @@
-enum IpAddrKind{
-    V4,
-    V6
+enum IpAddr{
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-
-    fn route(IpAddrKind::V4);
-    fn route(IpAddrKind::V6);
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
 
 }
 

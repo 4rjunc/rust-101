c8665a9cb40c245a60b413896b5eef22a8209e29
4rjunc <arjunmundamani@gmail.com>
2025-04-04 20:03:38 +0530

    Unsafe

--- unsafe_rs/src/main.rs
+++ unsafe_rs/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut num = 5;
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        print!("r1 is: {}", *r1);
+        print!("r2 is: {}", *r2);
+    }
 }

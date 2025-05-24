f1f9e280e15e639e69dce8fe5046cf8fdb9ba221
4rjunc <arjunmundamani@gmail.com>
2025-05-24 18:16:49 +0530

    Shadow

--- shadowing/src/main.rs
+++ shadowing/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
+    // Initial value
     let x = 5;
-    println!("Initial value of x: {}", x);
+    println!("Original x: {}", x);
 
     // Shadowing x with a new value
     let x = x + 1;
-    println!("After adding 1: {}", x);
+    println!("After increment: {}", x);
 
-    // Shadowing x again with a different type
+    // Shadowing again with a new type
     let x = format!("x is now a string: {}", x);
     println!("{}", x);
 }

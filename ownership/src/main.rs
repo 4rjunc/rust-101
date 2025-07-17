7070efa702cdd66c2acb69a4429cd8966eca8b51
4rjunc <arjunmundamani@gmail.com>
2025-07-17 14:28:01 +0530

    test

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -8,22 +8,22 @@ fn main() {
     println!("x:{}", x);
     // this print is invalid since the ownership of s is tranfered to the fn.
     //println!("s: {}", s);
-    
+
     //function returning multiple values
     let message = String::from("Im Batman");
     let (msg, len) = calculate_msg(message);
     println!("message:{} has length:{}", msg, len);
 }
 
-fn calculate_msg(msg: String) -> (String, usize){
+fn calculate_msg(msg: String) -> (String, usize) {
     let length = msg.len();
 
     (msg, length)
 }
-fn take_ownership(str: String){
+fn take_ownership(str: String) {
     println!("str: {}", str);
 }
 
-fn make_a_copy(num: i32){
+fn make_a_copy(num: i32) {
     println!("num:{}", num);
 }
